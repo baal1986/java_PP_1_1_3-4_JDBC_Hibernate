4f1787ad377906fa85bd11bef2952cd11aabd297
Alexei Bakholdin <aleksey.baholdin@gmail.com>
2023-06-03 15:01:01 +0300

    add config.properties

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,9 +3,14 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
+import java.util.Properties;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+
+
     public UserDaoJDBCImpl() {
+
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,13 @@
 package jm.task.core.jdbc.util;
 
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final Properties properties = new Properties();
+    private static final String dataBaseHost = properties.getProperty("db.host");
+    private static final String dataBaseLogin = properties.getProperty("db.login");
+    private static final String dataBasePassword = properties.getProperty("db.password");
+
+
+
 }
