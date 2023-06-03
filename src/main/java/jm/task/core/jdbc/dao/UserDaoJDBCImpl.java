9158d12e635150b20c286398c5f5b495e2a288a5
Alexei Bakholdin <aleksey.baholdin@gmail.com>
2023-06-03 17:03:29 +0300

    add .gitignore

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
-import java.util.Properties;
+
 
 public class UserDaoJDBCImpl implements UserDao {
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Optional;
 import java.util.Properties;
 
 public class Util {
@@ -7,7 +11,15 @@ public class Util {
     private static final String dataBaseHost = properties.getProperty("db.host");
     private static final String dataBaseLogin = properties.getProperty("db.login");
     private static final String dataBasePassword = properties.getProperty("db.password");
+    private static Optional<Connection> connection;
 
+    private static Optional<Connection> Connecting() throws ClassNotFoundException, SQLException {
+        Class.forName("jm.task.mysql.Driver");
+        return Optional.ofNullable(
+                DriverManager.getConnection(dataBaseHost, dataBaseLogin, dataBasePassword));
+    }
 
-
+    public static Optional<Connection> getConnection() throws ClassNotFoundException, SQLException {
+        return connection.isPresent() ? connection : Connecting();
+    }
 }
