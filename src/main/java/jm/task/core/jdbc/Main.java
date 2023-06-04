b3d18003b3141d4ecc321a814ae9a6ad41b2bdfa
Alexei Bakholdin <aleksey.baholdin@gmail.com>
2023-06-04 11:52:18 +0300

    refactor: class Util

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,15 +2,17 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.util.Util;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            System.out.printf(new Util().toString());
-            System.out.printf(String.valueOf(Util.getConnection().isClosed()));
-        } catch (ClassNotFoundException | SQLException ignored) {
-
+            Util util = new Util();
+            System.out.println(util.toString());
+            System.out.printf(String.valueOf(util.getConnection().isClosed()));
+        } catch (ClassNotFoundException | SQLException | IOException ignored) {
+            System.out.printf(String.valueOf(ignored.fillInStackTrace()));
         }
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,33 +1,54 @@
 package jm.task.core.jdbc.util;
 
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.regex.Pattern;
+
 
 public class Util {
     private static final Properties properties = new Properties();
-    private static final String dataBaseHost = properties.getProperty("db.host");
-    private static final String dataBaseLogin = properties.getProperty("db.login");
-    private static final String dataBasePassword = properties.getProperty("db.password");
+    private static String dataBaseDriver = properties.getProperty("db.driver");
+    private static String dataBaseHost = properties.getProperty("db.host");
+    private static String dataBaseLogin = properties.getProperty("db.login");
+    private static String dataBasePassword = properties.getProperty("db.password");
     private static Optional<Connection> connection;
 
-    private static Optional<Connection> Connecting() throws ClassNotFoundException, SQLException {
+    public Util() throws IOException {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("database.properties")) {
+
+            if (inputStream != null) {
+                properties.load(inputStream);
+            } else {
+                throw new FileNotFoundException();
+            }
+
+            dataBaseDriver = properties.getProperty("db.driver");
+            dataBaseHost = properties.getProperty("db.host");
+            dataBaseLogin = properties.getProperty("db.login");
+            dataBasePassword = properties.getProperty("db.password");
+        }
+    }
+
+    private Optional<Connection> Connecting() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = Optional.ofNullable(
                 DriverManager.getConnection(dataBaseHost, dataBaseLogin, dataBasePassword));
         return connection;
     }
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Connecting();
         return connection.isPresent() ? connection.get() : Connecting().orElse(null);
     }
 
     @Override
-    public  String toString() {
-        return dataBaseHost + " " + dataBaseLogin + " " + dataBasePassword;
+    public String toString() {
+        return dataBaseDriver + " " + dataBaseHost + " " + dataBaseLogin + " " + dataBasePassword;
     }
 }
