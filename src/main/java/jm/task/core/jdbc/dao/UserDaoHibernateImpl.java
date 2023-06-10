71f4ef647ff443ca0264ef658960ad0e43fe79c3
Alexei Bakholdin <aleksey.baholdin@gmail.com>
2023-06-10 12:02:55 +0300

    add: UserDaoHibernateImpl::createUsersTable() add: UserDaoHibernateImpl::dropUsersTable()

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -18,7 +18,7 @@ public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
         try {
             sessionFactory = new Util().getSessionFactory();
-        } catch (SQLException | IOException | ClassNotFoundException e) {
+        } catch (HibernateException | SQLException | ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
     }
@@ -26,12 +26,37 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
 
+            session.createSQLQuery(
+                            "CREATE TABLE IF NOT EXISTS users(" +
+                                    "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+                                    "name VARCHAR(250) NOT NULL," +
+                                    "lastName VARCHAR(250) NOT NULL," +
+                                    "age TINYINT UNSIGNED NOT NULL);"
+                    ).addEntity(User.class)
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
 
+            session.createSQLQuery("DROP TABLE IF EXISTS users CASCADE;")
+                    .addEntity(User.class)
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+        }
     }
 
     @Override
@@ -47,21 +72,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
 
-            session.createSQLQuery(
-                    "CREATE TABLE IF NOT EXISTS users(" +
-                            "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
-                            "name VARCHAR(250) NOT NULL," +
-                            "lastName VARCHAR(250) NOT NULL," +
-                            "age TINYINT UNSIGNED NOT NULL);"
-            ).addEntity(User.class);
-
-            session.getTransaction().commit();
-        } catch (HibernateException hibernateException) {
-            hibernateException.printStackTrace();
-        }
         return users;
     }
 
