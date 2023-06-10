66c3ec9d9c833d2cf3e47d335f48be3b793eff20
Alexei Bakholdin <aleksey.baholdin@gmail.com>
2023-06-10 11:23:58 +0300

    add: UserDaoJDBCImpl::createUsersTable()

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,14 +1,26 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
 
+    private SessionFactory sessionFactory;
 
     public UserDaoHibernateImpl() {
-
+        try {
+            sessionFactory = new Util().getSessionFactory();
+        } catch (SQLException | IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -34,7 +46,23 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+
+            session.createSQLQuery(
+                    "CREATE TABLE IF NOT EXISTS users(" +
+                            "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+                            "name VARCHAR(250) NOT NULL," +
+                            "lastName VARCHAR(250) NOT NULL," +
+                            "age TINYINT UNSIGNED NOT NULL);"
+            ).addEntity(User.class);
+
+            session.getTransaction().commit();
+        } catch (HibernateException hibernateException) {
+            hibernateException.printStackTrace();
+        }
+        return users;
     }
 
     @Override
